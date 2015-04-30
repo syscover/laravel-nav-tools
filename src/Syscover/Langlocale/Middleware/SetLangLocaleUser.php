6314c20b605cb414cc22efc9196bcc6a4489a2a0
Jose Carlos <cpalacin@syscover.com>
2015-05-01 01:36:57 +0200

    various

--- src/Syscover/Langlocale/Middleware/SetLangLocaleUser.php
+++ src/Syscover/Langlocale/Middleware/SetLangLocaleUser.php
@@ -22,6 +22,10 @@ class SetLangLocaleUser {
             {
                 $langLocaleData = explode("-", $request->segment(1));
             }
+            elseif(config('langlocale.urlType') == 'lang' || onfig('langlocale.urlType') == 'locale')
+            {
+                $langLocaleData = $request->segment(1);
+            }
         }
         else
         {
@@ -32,67 +36,90 @@ class SetLangLocaleUser {
         // routine to establish country and language variables in session, with URL data language and country
         if (config('langlocale.urlType') == 'langlocale' && count($langLocaleData) == 2 && in_array($langLocaleData[0], config('langlocale.langs')) && in_array($langLocaleData[1], config('langlocale.countries')))
         {
-            session('langUser',     $langLocaleData[0]);
-            session('countryUser',  $langLocaleData[1]);
+            session(['langUser'     => $langLocaleData[0]]);
+            session(['countryUser'  => $langLocaleData[1]]);
         }
-
-        // routine to set variables if we have cookies, set in session variables
-        elseif(cookie('langUser') != null && cookie('countryUser') != null)
+        // when only we need know user language
+        elseif(config('langlocale.urlType') == 'lang' && in_array($langLocaleData, config('langlocale.langs')))
         {
-            session('langUser',     cookie('langUser'));
-            session('countryUser',  cookie('countryUser'));
+            session(['langUser' => $langLocaleData]);
+        }
+        // when only we need know user country
+        elseif(config('langlocale.urlType') == 'locale' && in_array($langLocaleData, config('langlocale.countries')))
+        {
+            session(['countryUser' => $langLocaleData]);
+        }
+        // routine to set variables if we have cookies, set in session variables
+        elseif($request->cookie('langUser') != null && $request->cookie('countryUser') != null)
+        {
+            session('langUser',     $request->cookie('langUser'));
+            session('countryUser',  $request->cookie('countryUser'));
         }
 
         // routine to set session variables without cookies
-        elseif (session('langUser') == null || sessison('countryUser') == null)
+        elseif(session('langUser') == null || session('countryUser') == null)
         {
-            // Routine to know language
-            // get header HTTP_ACCEPT_LANGUAGE if there is this variable,
-            // the bots like google don't have this variable, in this case we have to complete language data.
-
-            if (isset($_SERVER['HTTP_ACCEPT_LANGUAGE']))
+            if(config('langlocale.urlType') == 'langlocale' || config('langlocale.urlType') == 'lang')
             {
-                $browserLang = Syscover\Langlocale\Libraries\Miscellaneous::preferedLanguage(config('langlocale.langs'));
-
-                // instantiate browser language
-                if(in_array($browserLang, config('langlocale.langs')))
+                // Routine to know language
+                // get header HTTP_ACCEPT_LANGUAGE if there is this variable,
+                // the bots like google don't have this variable, in this case we have to complete language data.
+                if (isset($_SERVER['HTTP_ACCEPT_LANGUAGE']))
                 {
-                    $lang = $browserLang;
+                    $browserLang = \Syscover\Langlocale\Libraries\Miscellaneous::preferedLanguage(config('langlocale.langs'));
+
+                    // instantiate browser language
+                    if(in_array($browserLang, config('langlocale.langs')))
+                    {
+                        $lang = $browserLang;
+                    }
+                    else
+                    {
+                        $lang = config('app.locale');
+                    }
                 }
                 else
                 {
+                    // in this case, ser default lang
                     $lang = config('app.locale');
                 }
-            }
-            else
-            {
-                // in this case, ser default lang
-                $lang = config('app.locale');
+
+                session(['langUser' => $lang]);
             }
 
-            // Routine to know country
-            // We find out the client's IP
-            $ip = Syscover\Langlocale\Libraries\Miscellaneous::getRealIp();
-            $browserCountry = Syscover\Langlocale\Libraries\Miscellaneous::getCountryIp($ip);
 
-            if (in_array($browserCountry, config('langlocale.countries')))
+            if(config('langlocale.urlType') == 'langlocale' || config('langlocale.urlType') == 'locale')
             {
-                $country = $browserCountry;
-            }
-            else
-            {
-                // in the case of not getting a valid country, we take the country as default language
-                $country = config('langlocale.countryLang')[$lang];
-            }
+                // Routine to know country
+                // We find out the client's IP
+                $ip = \Syscover\Langlocale\Libraries\Miscellaneous::getRealIp();
+                $browserCountry = \Syscover\Langlocale\Libraries\Miscellaneous::getCountryIp($ip);
 
-            session('langUser',     $lang);
-            session('countryUser',  $country);
+                if (in_array($browserCountry, config('langlocale.countries')))
+                {
+                    $country = $browserCountry;
+                }
+                // if is set locale, we get default country from locale
+                elseif(config('langlocale.urlType') == 'langlocale' || config('langlocale.urlType') == 'lang')
+                {
+                    // in the case of not getting a valid country, we take the country as default language
+                    $country = config('langlocale.countryLang')[$lang];
+                }
+                else
+                {
+                    $country = config('langlocale.defaultCountry');
+                }
+
+                session(['countryUser' => $country]);
+            }
         }
 
-        // we establish the language environment
-        echo('resultado: '.session('langUser'));
-        App::setLocale(session('langUser'));
+        if(config('langlocale.urlType') == 'langlocale' || config('langlocale.urlType') == 'lang')
+        {
+            // we establish the language environment
+            App::setLocale(session('langUser'));
+        }
 
-        //return $next($request);
+        return $next($request);
     }
 }
